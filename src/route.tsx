7a838b5924f96db1645e64e844e73cdd9bc405c8
Abhishek Rajput <rabhi0766@gmail.com>
2025-02-10 17:23:05 +0530

    initail project setup commit

---
+++ src/route.tsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import DashboardLayout from "./layouts/DashboardLayout";
+import BooksPage from "./pages/BooksPage";
+import AuthLayout from "./layouts/AuthLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "home",
+        element: <HomePage />,
+      },
+      {
+        path: "books",
+        element: <BooksPage />,
+      },
+    ],
+  },
+
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "register",
+        element: <RegisterPage />,
+      },
+    ],
+  },
+]);
+
+export default router;
